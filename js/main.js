e1e3963d712f14f7501b1e4e809a656719586c24
FernandoDLT <ferchodlt1971@gmail.com>
2025-10-20 15:34:06 -0700

    Refactored if condition to h2 vs alert

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
    const rapperName = document.querySelector('input').value.trim();
 
-      if(!rapperName){ // checks if field is empty
-         alert("Please enter a rapper's name")
-         return;
-      }
-
+   if (!rapperName) {
+      document.querySelector('h2').innerText = 'Please enter a rapper name';
+      return;
+   }
+   
       try{
       const response = await fetch(`https://client-side-code.onrender.com/api/${rapperName}`)
       const data = await response.json()
