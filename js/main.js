7a7c5a18030ebf0f02887fbdc079bfc316d11005
FernandoDLT <ferchodlt1971@gmail.com>
2025-10-20 15:22:56 -0700

    Moved if condition up, returns early with alert before calling API

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const rapperName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://client-side-code.onrender.com/api/${rapperName}`)
-       const data = await response.json()
-       
-       if(!rapperName){ // checks if field is empty
-        alert("Please enter a rapper's name")
-        return
-       }
+   const rapperName = document.querySelector('input').value.trim();
 
-        console.log(data)
-        document.querySelector('h2').innerText = data.birthName
-    }catch(error){
-        console.log(error)
-    }
+      if(!rapperName){ // checks if field is empty
+         alert("Please enter a rapper's name")
+         return;
+      }
+
+      try{
+      const response = await fetch(`https://client-side-code.onrender.com/api/${rapperName}`)
+      const data = await response.json()
+
+      console.log(data)
+         document.querySelector('h2').innerText = data.birthName
+         
+      }catch(error){ 
+      console.log(error)
+   }
 }
